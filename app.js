c295f433e4cd4e0d61a89d28605b5bd131d2b033
sm2424 <shivmodi6@gmail.com>
2024-02-06 11:58:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// app.js
+const express = require('express');
+const session = require('express-session');
+const routes = require('./routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use('/', routes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,66 @@
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post('/get-otp', (req, res) => {
+  const enteredMobileNumber = req.body.mobileNumber;
+
+  // Validate if the provided mobile number has exactly 10 digits and is '1234567890'
+  if (enteredMobileNumber === '1234567890') {
+    // Valid mobile number, generate and send OTP (skipping for simplicity)
+    req.session.mobileNumber = enteredMobileNumber;
+    res.render('otp');
+  } else {
+    res.render('login', { error: 'Invalid mobile number. Please enter the correct mobile number (1234567890).' });
+  }
+});
+
+router.post('/verify-otp', (req, res) => {
+  const enteredOTP = req.body.otp;
+
+  // Check if the provided OTP is valid
+  if (enteredOTP === '123456') {
+    // Valid OTP, user is authenticated
+    req.session.isAuthenticated = true;
+    req.session.username = 'shiv'; // Set username to 'shiv'
+    res.redirect('/dashboard');
+  } else {
+    res.render('otp', { error: 'Invalid OTP. Please try again.' });
+  }
+});
+
+router.get('/dashboard', (req, res) => {
+  // Check if the user is authenticated
+  if (req.session.isAuthenticated) {
+    const mobileNumber= req.session.mobileNumber;
+    res.render('dashboard', { username: req.session.username, mobileNumber: mobileNumber });
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.get('/aboutus', (req, res) => {
+  // Check if the user is authenticated
+  if (req.session.isAuthenticated) {
+    const mobileNumber= req.session.mobileNumber;
+    res.render('aboutus', { username: req.session.username, mobileNumber: mobileNumber });
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+module.exports = router;
